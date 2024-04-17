77fb4a4dbcc15ed9232e3262696407151b19069f
giabao1712 <151906192+giabao1712@users.noreply.github.com>
2024-04-17 21:01:06 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,?!'_-&@#$%*()/:<>|+= ";
+// Chức năng mã hóa
+function encryptText(text, key) {
+    let encryptedText = "";
+
+    for (let i = 0; i < text.length; i++) {
+        const textChar = text[i];
+        const keyChar = key[i % key.length];
+
+        const textIndex = alphabet.indexOf(textChar);
+        const keyIndex = alphabet.indexOf(keyChar);
+
+        if (textIndex === -1) {
+            encryptText += textChar;
+        } else {
+            const newIndex = (textIndex + keyIndex) % alphabet.length;
+            encryptedText += alphabet[newIndex];
+        }
+    }
+
+    return encryptedText;
+}
+// Hàm giải mã
+function decryptText(encryptedText, key) {
+    let decryptedText = "";
+
+    for (let i = 0; i < encryptedText.length; i++) {
+        const encryptedChar = encryptedText[i];
+        const keyChar = key[i % key.length];
+
+        const encryptedIndex = alphabet.indexOf(encryptedChar);
+        const keyIndex = alphabet.indexOf(keyChar);
+
+        if (encryptedText === -1) {
+            decryptedText += encryptedChar;
+        } else {
+            let newIndex = encryptedIndex - keyIndex;
+            if (newIndex < 0) newIndex += alphabet.length;
+            decryptedText += alphabet[newIndex];
+        }
+    }
+
+    return decryptedText;
+}
+// Cập nhật kết quả dựa trên thao tác đã chọn (enc hoặc dec)
+function updateResult(isEncrypting) {
+    const text = document.getElementById("message").value;
+    const key = document.getElementById("key").value;
+
+    let result = "";
+
+    if (isEncrypting) {
+        result = encryptText(text, key);
+    } else {
+        result = decryptText(text, key);
+    }
+
+    document.getElementById("result").textContent = result;
+}
+// Thêm trình xử lý sự kiện vào các nút
+document.getElementById("enc-btn").addEventListener('click', function () {
+    updateResult(true);
+});
+
+document.getElementById("dec-btn").addEventListener('click', function () {
+    updateResult(false);
+});
+// Khởi tạo kết quả bằng văn bản được mã hóa khi tải trang
+document.addEventListener('DOMContentLoaded', () => {
+    updateResult(true);
+});
